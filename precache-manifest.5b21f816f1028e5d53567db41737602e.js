57b37d1f585a22073685287b904444a7c1ac0158
Wonyoung So <wso@mit.edu>
2020-07-12 22:40:23 -0400

    Updates

---
+++ precache-manifest.5b21f816f1028e5d53567db41737602e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361e9d15d98be4345257abc4dc685c09",
+    "url": "/landlordtech/index.html"
+  },
+  {
+    "revision": "a55815be85dd2b6f1ffd",
+    "url": "/landlordtech/static/css/main.a28274e0.chunk.css"
+  },
+  {
+    "revision": "3d8f54d30659854b53a3",
+    "url": "/landlordtech/static/js/2.f60ae651.chunk.js"
+  },
+  {
+    "revision": "bcf5af6ee7b3d82fd45e0a1868aeaab1",
+    "url": "/landlordtech/static/js/2.f60ae651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55815be85dd2b6f1ffd",
+    "url": "/landlordtech/static/js/main.bd4ae8f8.chunk.js"
+  },
+  {
+    "revision": "fdf1cad7351bd8002067",
+    "url": "/landlordtech/static/js/runtime-main.31d0d67e.js"
+  }
+]);
