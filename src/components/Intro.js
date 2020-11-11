c8e9312cfd693ca4f9989e828642711d45393d58
Wonyoung So <wso@mit.edu>
2020-11-10 22:14:50 -0500

    update

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Container, Title, Gutter, Circle, Subtitle, Paragraph } from '../stylesheets/components';
 import styled from 'styled-components';
 import { Nomenclature } from './';
@@ -67,6 +67,10 @@ const LineArea = styled.div`
   margin: 25px 0;
 `;
 
+const HeroImage = styled.img`
+  width: 100%;
+`;
+
 
 class Intro extends Component {
   handleClick(id){
@@ -82,92 +86,96 @@ class Intro extends Component {
     let directTechList = _.filter(TECH_LIST, tl => tl.type === "direct");
     
     return (
-      <Container>
-        <a name="whatis"></a>
-        <Gutter h={100} />
-        <BlackTitle>
-          What is <br/>
-          Landlord Tech?
-        </BlackTitle>
-        <Gutter h={50} />
-        <Circle />
-        <Gutter h={50} />
-        
-        <BlackSubtitle>
-          By Landlord Tech, we mean technical products and platforms that have facilitated the merging of the technology and real estate industries in novel ways, particularly as they impact tenant housing. Our research has led to two major categories of what we are calling Landlord Tech: surveillance and speculation, both of which are tied up in gentrification. 
+      <Fragment>
+        <HeroImage src={`${process.env.PUBLIC_URL}/assets/hero_image.png`} alt="landlord tech illustration" />
+        <Container>
+          <a name="whatis"></a>
+          <Gutter h={100} />
+          <BlackTitle>
+            What is <br/>
+            Landlord Tech?
+          </BlackTitle>
+          <Gutter h={50} />
+          <Circle />
+          <Gutter h={50} />
+          
+          <BlackSubtitle>
+            By Landlord Tech, we mean technical products and platforms that have facilitated the merging of the technology and real estate industries in novel ways, particularly as they impact tenant housing. Our research has led to two major categories of what we are calling Landlord Tech: surveillance and speculation, both of which are tied up in gentrification. 
 
-        </BlackSubtitle>
-        <Gutter h={50} />
-        <BlackParagraph>
-          There are few laws and regulations governing the collection and use of data in the context of Landlord Tech. Because it is generally sold to landlords and property managers, not tenants, Landlord Tech is often installed without notifying or discussing potential harms with tenants and community members. These harms include the possibility that sensitive and personal data can be handed over to the police, ICE, or other law enforcement and government agencies. Landlord Tech can also be used to automate evictions, racial profiling, and tenant harassment. In addition, Landlord Tech is used to abet real estate speculation and gentrification, making buildings more desirable to whiter and wealthier tenants, while feeding real estate and tech companies with property - be that data or real estate. Landlord Tech tracking platforms have increasingly been <a href="http://bostonreview.net/class-inequality-science-nature/erin-mcelroy-meredith-whittaker-genevieve-fried-covid-19-crisis" target="_blank">marketed to landlords as solutions to Covid-19</a>, leading to new forms of residential surveillance. 
-{/*           
-          While landlord tech has celebrated the “disruption” of the real estate industry with new technology and data infrastructures, it often exacerbates housing inequality, racial profiling, speculation, and gentrification. We are particularly worried that landlord tech is leading to new forms of housing injustice in the wake of COVID-19 by amplifying surveillance, tracking, data accumulation, and algorithmic means testing into domestic and neighborhood spaces. */}
-        </BlackParagraph>
-         
-        <Gutter h={50} />
-        <ListTitle>
-          Here are examples of Landlord Tech. <br/>
-          Click on each to learn more about how these<br/>
-          technologies function, and <br/>
-          the potential harms.
-        </ListTitle>
-
-
-        <ListContainer>
-          <LineArea>
-            <svg width="2" height="69" viewBox="0 0 2 69" fill="none">
-              <path d="M1 0V69" stroke="black"/>
-            </svg>
-          </LineArea>
+          </BlackSubtitle>
+          <Gutter h={50} />
+          <BlackParagraph>
+            There are few laws and regulations governing the collection and use of data in the context of Landlord Tech. Because it is generally sold to landlords and property managers, not tenants, Landlord Tech is often installed without notifying or discussing potential harms with tenants and community members. These harms include the possibility that sensitive and personal data can be handed over to the police, ICE, or other law enforcement and government agencies. Landlord Tech can also be used to automate evictions, racial profiling, and tenant harassment. In addition, Landlord Tech is used to abet real estate speculation and gentrification, making buildings more desirable to whiter and wealthier tenants, while feeding real estate and tech companies with property - be that data or real estate. Landlord Tech tracking platforms have increasingly been <a href="http://bostonreview.net/class-inequality-science-nature/erin-mcelroy-meredith-whittaker-genevieve-fried-covid-19-crisis" target="_blank">marketed to landlords as solutions to Covid-19</a>, leading to new forms of residential surveillance. 
+  {/*           
+            While landlord tech has celebrated the “disruption” of the real estate industry with new technology and data infrastructures, it often exacerbates housing inequality, racial profiling, speculation, and gentrification. We are particularly worried that landlord tech is leading to new forms of housing injustice in the wake of COVID-19 by amplifying surveillance, tracking, data accumulation, and algorithmic means testing into domestic and neighborhood spaces. */}
+          </BlackParagraph>
+          
+          <Gutter h={50} />
           <ListTitle>
-            Surveillance Tech (Direct Impact to Tenants):
+            Here are examples of Landlord Tech. <br/>
+            Click on each to learn more about how these<br/>
+            technologies function, and <br/>
+            the potential harms.
           </ListTitle>
+
+
+          <ListContainer>
+            <LineArea>
+              <svg width="2" height="69" viewBox="0 0 2 69" fill="none">
+                <path d="M1 0V69" stroke="black"/>
+              </svg>
+            </LineArea>
+            <ListTitle>
+              Surveillance Tech (Direct Impact to Tenants):
+            </ListTitle>
+            {
+              _.map (directTechList, tech => {
+                // eslint-disable-next-line no-unused-expressions
+                return (
+                  <TechLink key={tech.id} href="javascript:void();" onClick={this.handleClick.bind(this, tech.id)}>
+                    { tech.title }
+                  </TechLink>
+                )
+              })
+            }
+
+            <LineArea>
+              <svg width="2" height="50" viewBox="0 0 2 50" fill="none">
+                <path d="M1 0V150" stroke="black"/>
+              </svg>
+            </LineArea>
+
+            <ListTitle>
+              Speculation Tech (Indirect Impact to Tenants):
+            </ListTitle>
+            {
+              _.map (indirectTechList, tech => {
+                // eslint-disable-next-line no-unused-expressions
+                return (
+                  <TechLink key={tech.id} href="javascript:void();" onClick={this.handleClick.bind(this, tech.id)}>
+                    { tech.title }
+                  </TechLink>
+                )
+              })
+            }
+
+            <LineArea>
+              <svg width="2" height="150" viewBox="0 0 2 150" fill="none">
+                <path d="M1 0V150" stroke="black"/>
+              </svg>
+            </LineArea> 
+              
+          </ListContainer>
+          
+          <Gutter h={100} />
+
           {
-            _.map (directTechList, tech => {
-              // eslint-disable-next-line no-unused-expressions
-              return (
-                <TechLink key={tech.id} href="javascript:void();" onClick={this.handleClick.bind(this, tech.id)}>
-                  { tech.title }
-                </TechLink>
-              )
-            })
+            currentNomenclature ? 
+            <Nomenclature /> : null
           }
-
-          <LineArea>
-            <svg width="2" height="50" viewBox="0 0 2 50" fill="none">
-              <path d="M1 0V150" stroke="black"/>
-            </svg>
-          </LineArea>
-
-          <ListTitle>
-            Speculation Tech (Indirect Impact to Tenants):
-          </ListTitle>
-          {
-            _.map (indirectTechList, tech => {
-              // eslint-disable-next-line no-unused-expressions
-              return (
-                <TechLink key={tech.id} href="javascript:void();" onClick={this.handleClick.bind(this, tech.id)}>
-                  { tech.title }
-                </TechLink>
-              )
-            })
-          }
-
-          <LineArea>
-            <svg width="2" height="150" viewBox="0 0 2 150" fill="none">
-              <path d="M1 0V150" stroke="black"/>
-            </svg>
-          </LineArea> 
-            
-        </ListContainer>
-        
-        <Gutter h={100} />
-
-        {
-          currentNomenclature ? 
-          <Nomenclature /> : null
-        }
-      </Container>
+        </Container>
+      </Fragment>
+      
     )
   }
 }
